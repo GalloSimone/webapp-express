13240288edd9b5fb393ce8b4919e4fdd98ba6954
GalloSimone <gallosimonecz@gmail.com>
2025-01-10 16:47:08 +0100

    modifiche all'app express

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require ('express');
 
 const app = express();
 
+const{APP_HOST,APP_PORT}=process.env
 
+app.get("/",(req,res)=>{
+    res.json({
+       meassage:"OK" 
+    });
+})
 
-app.listen(3000,()=>{
-    console.log('server in ascolto su http://localhost:3000')
+app.listen(APP_PORT,()=>{
+    console.log(`server in ascolto su ${APP_HOST}:${APP_PORT}`)
 })
